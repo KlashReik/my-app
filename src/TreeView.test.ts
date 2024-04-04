03c2f945c5f51f0bac09b264fbfc516be2597acb
Ivan Bulavko <ivanb@moduleworks.com>
2024-04-04 16:58:04 +0300

    feat: add userEvent

--- src/TreeView.test.ts
+++ src/TreeView.test.ts
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/svelte';
-import { test, expect } from 'vitest';
+import userEvent from '@testing-library/user-event';
+import { expect, test } from 'vitest';
 import TreeView from './TreeView.svelte';
 
 const TREE_MOCK = {
@@ -27,7 +28,7 @@ const TREE_MOCK = {
 	]
 };
 
-test('Folder Tree correctly opens nested folders', async () => {
+test('TreeView correctly opens nested folders', async () => {
 	render(TreeView, { tree: TREE_MOCK });
 	const rootItem = screen.getByText('USA');
 
@@ -37,15 +38,30 @@ test('Folder Tree correctly opens nested folders', async () => {
 	const firstLevelFolder = screen.getByText('Florida');
 	expect(firstLevelFolder).toBeInTheDocument();
 });
-// TODO: this logic should change
 
-test('Folder Tree correctly renders empty folders', async () => {
+test('ThreeView the same as previous', async () => {
 	render(TreeView, { tree: TREE_MOCK });
 	const rootItem = screen.getByText('USA');
 
 	expect(rootItem).toBeInTheDocument();
 
 	await fireEvent.click(rootItem);
+	// Line below makes test pass
+	await fireEvent.click(rootItem);
+
+	const firstLevelFolder = screen.getByText('Florida');
+	expect(firstLevelFolder).toBeInTheDocument();
+});
+
+test('ThreeView the same as previous but with userEvent', async () => {
+	const user = userEvent.setup();
+	render(TreeView, { tree: TREE_MOCK });
+	const rootItem = screen.getByText('USA');
+
+	expect(rootItem).toBeInTheDocument();
+
+	// works, BUT if you remove previous test or make it pass -> this one fails
+	await user.click(rootItem);
 	// Line below makes test pass
 	// await fireEvent.click(rootItem);
 
